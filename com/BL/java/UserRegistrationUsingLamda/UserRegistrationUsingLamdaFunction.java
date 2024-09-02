a1d4bb7e0c9e7c4a4998e58cba43dcf22b0582e4
sahil belnekar <sahilsbelnekar2002@gmail.com>
2024-09-02 12:34:20 +0530

    validate email with address

--- com/BL/java/UserRegistrationUsingLamda/UserRegistrationUsingLamdaFunction.java
+++ com/BL/java/UserRegistrationUsingLamda/UserRegistrationUsingLamdaFunction.java
@@ -16,6 +16,11 @@ public class UserRegistrationUsingLamdaFunction {
          return Pattern.matches("^[A-Z][a-zA-Z]{2,}$", LastName);
      }
 
+  //UC3 Enter the valid email
+
+     public static Boolean ValidEmail (String Email){
+         return Pattern.matches("^[a-zA-Z0-9]+([._%+-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+\\.[a-zA-Z]{2,6}(\\.[a-zA-Z]{2,6})?$", Email);
+     }
 
 
  }
